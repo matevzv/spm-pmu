a3672be333869422d8651a9fada3d3233a6dfd0b
Matevz Vucnik <vucnikm@gmail.com>
2017-02-06 14:16:10 +0100

    proper one queue per client implementation

--- pmu.py
+++ pmu.py
@@ -2,19 +2,21 @@ import socketserver
 
 from queue import Queue
 from threading import Thread
-from threading import Lock
 from synchrophasor.frame import *
 from datetime import datetime
 
 class Service(socketserver.BaseRequestHandler):
     queue = None
-    sending_measurements_enabled = False
+    queues = None
     header = None
     cfg2 = None
     socket_open = True
+    sending_measurements_enabled = False
+    client_limit = 10
 
     def __init__(self, request, client_address, server):
-        self.queue = server.queue
+        self.queue = Queue()
+        self.queues = server.queues
         self.header = server.header
         self.cfg2 = server.cfg2
         socketserver.BaseRequestHandler.__init__(self, request, client_address, server)
@@ -25,7 +27,6 @@ class Service(socketserver.BaseRequestHandler):
 
     def pmu_handler(self, received_data):
         command = None
-        response = b''
 
         received_message = CommonFrame.convert2frame(received_data)
         command = received_message.get_command()
@@ -41,18 +42,23 @@ class Service(socketserver.BaseRequestHandler):
             time = self.get_time()
             self.header.set_time(time[0], time[1])
             response = self.header.convert2bytes()
+            self.queue.put_nowait(['command', response])
 
         elif command == 'cfg2':
             time = self.get_time()
             self.cfg2.set_time(time[0], time[1])
             response = self.cfg2.convert2bytes()
-
-        return response
+            self.queue.put_nowait(['command', response])
 
     def handle(self):
         print("Client connected with " + str(self.client_address))
 
-        lock = Lock()
+        if len(self.queues) < self.client_limit:
+            self.queues.append(self.queue)
+        else:
+            print("Client limit exceeded: " + str(self.client_limit))
+            print("Client exited with " + str(self.client_address))
+            return
 
         t = Thread(target = self.send_data, args=[])
         t.start()
@@ -66,36 +72,44 @@ class Service(socketserver.BaseRequestHandler):
                 break
 
             try:
-                byts = self.pmu_handler(data)
-                with lock:
-                    self.request.sendall(byts)
+                self.pmu_handler(data)
             except:
                 continue
 
+        self.queues.remove(self.queue)
         self.socket_open = False
+        self.queue.put(None)
         t.join()
+
         print("Client exited with " + str(self.client_address))
 
     def send_data(self):
         while self.socket_open:
             send_data = self.queue.get()
-            if self.sending_measurements_enabled == True:
-                try:
-                    self.request.sendall(send_data)
-                except:
+            try:
+                if not isinstance(send_data, list):
+                    if self.sending_measurements_enabled == False or send_data == None:
+                        continue
+                elif send_data[0] != 'command':
                     continue
-                finally:
-                    self.queue.task_done()
+                else:
+                    send_data = send_data[1]
+
+                self.request.sendall(send_data)
+            except:
+                continue
+            finally:
+                self.queue.task_done()
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True,
-                 queue=None):
-        self.queue = queue
+                 queues=None):
+        self.queues = queues
         socketserver.TCPServer.__init__(self, server_address, Service,
                                         bind_and_activate=bind_and_activate)
 
 class Pmu():
-    queue = Queue()
+    queues = []
     pmu_id = None
     header = None
     cfg2 = None
@@ -108,7 +122,7 @@ class Pmu():
         self.num_pmu = cfg2.num_pmu
         self.data_format = cfg2.data_format
         socketserver.TCPServer.allow_reuse_address = True
-        server = ThreadedTCPServer((ip, port), Service, queue=self.queue)
+        server = ThreadedTCPServer((ip, port), Service, queues=self.queues)
         server.header = header
         server.cfg2 = cfg2
         Thread(target = server.serve_forever, args=[]).start()
@@ -121,4 +135,5 @@ class Pmu():
                             self.data_format, self.num_pmu)
 
         data_frame.set_time(soc, frasec)
-        self.queue.put_nowait(data_frame.convert2bytes())
+        for q in self.queues:
+            q.put_nowait(data_frame.convert2bytes())
